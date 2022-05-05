10448036c47fd8be02bb61dfb7092ae42e3983d6
Emrul-Hasan <imrulhasan003@gmail.com>
2022-05-06 02:28:24 +0600

    done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>This is About us</h2>
+            <h2> About page under construction</h2>
         </div>
     );
 };
